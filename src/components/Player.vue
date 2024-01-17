6edfe788ca55cdef3bc0192aff7079f8ae31421c
Antoine Lauzis <antoine.lauzis@etudiant.univ-reims.fr>
2024-01-17 19:14:30 +0100

    wip : player component

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -35,23 +35,36 @@
 
         methods: {
             togglePlayer() {
-                    this.isPlayerActive = !this.isPlayerActive;
+                // Bascule l'état d'activation du player : si actif, le désactive, et vice versa.
+                this.isPlayerActive = !this.isPlayerActive;
+
+                // Si le player est maintenant actif :
                 if (this.isPlayerActive) {
+                    // Joue l'audio 'before'.
                     this.$refs.beforeAudio.play();
+                    // Joue également l'audio 'after'.
                     this.$refs.afterAudio.play();
+                    // Assure que l'audio 'before' n'est pas en mode muet.
                     this.$refs.beforeAudio.muted = false;
+                    // Met l'audio 'after' en mode muet.
                     this.$refs.afterAudio.muted = true;
                 } else {
+                    // Si le player n'est pas actif, réinitialise l'audio 'before'.
                     this.resetAudio(this.$refs.beforeAudio);
+                    // Réinitialise également l'audio 'after'.
                     this.resetAudio(this.$refs.afterAudio);
                 }
             },
             triggerPlayer(type) {
+                // Définit si le mode 'before' est actif basé sur le type passé en argument.
                 this.isBeforeActive = type === 'before';
+                // Met l'audio 'before' en mode muet si le mode 'before' n'est pas actif.
                 this.$refs.beforeAudio.muted = !this.isBeforeActive;
+                // Met l'audio 'after' en mode muet si le mode 'before' est actif.
                 this.$refs.afterAudio.muted = this.isBeforeActive;
             },
             resetAudio(audioEl) {
+                // Arrête la lecture de l'audio et remet à zéro son temps.
                 audioEl.pause();
                 audioEl.currentTime = 0;
             },
