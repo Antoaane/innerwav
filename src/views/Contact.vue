d8ef6c94239f3f83e60ba42c4e687301d6458c4f
Antoine Lauzis <antoine.lauzis@etudiant.univ-reims.fr>
2024-02-14 17:13:49 +0100

    wip : contact style

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -4,19 +4,38 @@
 
 <template>
     <div id="contact">
+
         <div class="bg"></div>
+
         <div class="container">
             <form action="">
-                <label for="name">Votre nom :</label>
-                <input id="name" name="name" placeholder="Nassim Mouloud" type="text">
-                <label for="artist-name">Votre nom de scène :</label>
-                <input id="artist-name" name="artist-name" placeholder="Nassim 3x filtré" type="text">
-                <label for="email">Votre email :</label>
-                <input id="email" name="email" placeholder="nassim@filtrage-quali.bif" type="email">
-                <label for="message">Votre message :</label>
-                <textarea id="message" name="message" placeholder="Dites-nous tout !"></textarea>
-                <input type="submit" value="Envoyer">
+
+                <div>
+                    <label for="name">Votre nom :</label>
+                    <input id="name" name="name" type="text">
+                </div>
+                
+                <div>
+                    <label for="artist-name">Votre nom de scène :</label>
+                    <input id="artist-name" name="artist-name" type="text">
+                </div>
+                
+                <div>
+                    <label for="email">Votre email :</label>
+                    <input id="email" name="email" type="email">
+                </div>
+                
+                <div>
+                    <label for="message">Votre message :</label>
+                    <textarea id="message" name="message"></textarea>
+                </div>
+                
+                <div>
+                    <input type="submit" value="Envoyer">
+                </div>
+                
             </form>
         </div>
+
     </div>
 </template>
