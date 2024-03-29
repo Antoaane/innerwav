4fcd6a534ded11f2d656a433e0b3e10f6d585316
Antoine Lauzis <antoine.lauzis@etudiant.univ-reims.fr>
2024-03-29 11:11:14 +0100

    wip : login page created

---
+++ src/views/Login.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue';
+
+const name = ref('');
+const artistName = ref('');
+const email = ref('');
+const password = ref('');
+</script>
+
+<template>
+    <form action="">
+
+        <div>
+            <input id="name" name="name" type="text" autocomplete="none" v-model="name">
+            <label for="name">Nom/Prénom :</label>
+        </div>
+
+        <div>
+            <input id="artist-name" name="artist-name" type="text" v-model="artistName">
+            <label for="artist-name">Nom de scène :</label>
+        </div>
+
+        <div>
+            <input id="email" name="email" type="email" v-model="email">
+            <label for="email">E-mail :</label>
+        </div>
+
+        <div>
+            <textarea id="password" name="password" v-model="password"></textarea>
+            <label for="password">Mot de passe :</label>
+        </div>
+
+        <div>
+            <input type="submit" value="Envoyer">
+        </div>
+
+    </form>
+</template>
