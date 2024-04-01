70a332a87cbe6e2eb590c921bfae91cb464b9794
Antoine Lauzis <antoine.lauzis@etudiant.univ-reims.fr>
2024-04-01 17:19:26 +0200

    feat : question scroll slider op

---
+++ src/views/MasteringSections/QuestionsComponent.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="tab question">
+        <div class="container">
+            <div class="spec-1">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div class="options">
+                <button>Single</button>
+                <button>EP</button>
+                <button>Album</button>
+            </div>
+            <div class="spec-2">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    </div>
+    <div class="tab question">
+        <div class="container bg-red-500">
+
+        </div>
+    </div>
+    <div class="tab question">
+        <div class="container bg-green-500">
+
+        </div>
+    </div>
+</template>
--- src/views/Masteriser.vue
+++ src/views/Masteriser.vue
@@ -1,21 +1,52 @@
 <script setup>
+import { ref } from 'vue';
 import CheckoutSection from './MasteringSections/CheckoutSection.vue'
-import QuestionComponent from './MasteringSections/QuestionComponent.vue';
+import QuestionsComponent from './MasteringSections/QuestionsComponent.vue';
+
+const scrollLevel = ref(0);
+
+function scrollNext() {
+    scrollLevel.value = scrollLevel.value + window.innerWidth;
+
+    document.getElementById('track').scrollTo({
+        left: scrollLevel.value,
+        behavior: 'smooth'
+    });
+}
+function scrollPrev() {
+    if (scrollLevel.value > 0) {
+        scrollLevel.value = scrollLevel.value - window.innerWidth;
+    }
+
+    document.getElementById('track').scrollTo({
+        left: scrollLevel.value,
+        behavior: 'smooth'
+    });
+}
 
 </script>
 
 <template>
     <div id="masteriser">
 
+        <h1 class="absolute -z-10 opacity-0">Masteriser</h1>
+
         <div class="bg"></div>
 
-        <div class="track">
-            <div class="container tab">
-                <QuestionComponent />
+        <div id="track">
+            <div id="train">
+                <QuestionsComponent />
+                <div class="tab">
+                    <div class="container">
+                        <CheckoutSection />
+                    </div>
+                </div>
             </div>
-            <div class="container tab">
-                <CheckoutSection />
-            </div>
+        </div>
+
+        <div class="absolute z-50 bottom-20 left-1/2 -translate-x-1/2">
+            <button @click="scrollPrev" class="p-2 rounded-full shadow-lg">Prev</button>
+            <button @click="scrollNext" class="p-2 rounded-full shadow-lg">Next</button>
         </div>
     </div>
 </template>
